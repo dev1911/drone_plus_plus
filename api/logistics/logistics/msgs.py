2db0635c23aa1b207879eaebd775f648b588e2e5
NikhilMJagtap <44296369+NikhilMJagtap@users.noreply.github.com>
2020-02-13 20:35:11 +0530

    added drones, warehouse and doc in logistics

---
+++ api/logistics/logistics/msgs.py
@@ -0,0 +1,11 @@
+no_user_token = "User token not found in the request."
+invalid_user_token = "Invalid user token found in the request"
+no_battery = "Battery not found in the request."
+invalid_warehouse = "Invalid warehouse name was passed in the request"
+no_drone_id = "Drone ID not found in the request"
+invalid_drone_id = "Invalid Drone ID was passed in request"
+no_drone_status = "No status parameter passed in request"
+invalid_status_choice = "Invalid Drone Status was passed in request"
+lat_long_missing = "Latitude and Longitude both should be passed to update the location."
+
+drone_status = ["Idle", "Charging", "Delivering", "Returning", "Offline"]
