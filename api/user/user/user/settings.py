96ae3a908f9257ed340170fa9048ee920b006db8
Devansh Shah <43884681+dev1911@users.noreply.github.com>
2020-04-27 19:58:07 +0530

    Added user dockerfile

--- api/user/user/user/settings.py
+++ api/user/user/user/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'user.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user',
+        'USER': os.getenv("MYSQL_USER" ,"admin"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD" , "admin"),
+        'HOST': os.getenv("MYSQL_USER_SERVICE_HOST"),
+        'PORT': os.getenv("MYSQL_USER_SERVICE_PORT"),
     }
 }
 AUTH_USER_MODEL = 'accounts.User'
