88f09dbaeff1bb8ca0920bd08be098c024dbbc92
Shloka Shah <shloka.shah@spit.ac.in>
2020-05-02 16:28:48 +0530

    Update urls.py

--- api/logistics/drone/urls.py
+++ api/logistics/drone/urls.py
@@ -4,8 +4,8 @@ from drone.views import *
 urlpatterns = [
     path('create/', create_drone, name='create_drone'),
     path('list/', list_drones, name='list_drones'),
-    path('info/', info_drone, name='info_drone'),
-    path('current-battery/', current_battery_drone, name='current_battery_drone'),
+    path('info/<str:drone_id>/', info_drone, name='info_drone'),
+    path('current-battery/<str:drone_id>/', current_battery_drone, name='current_battery_drone'),
     path('change-status/', change_status, name="change_status"),
     path('change-location/', change_location, name="change_location"),
 ]
