e44e42fa58da669725f49e40f40d7e7530392c35
dev1911 <shahdevansh99@gmail.com>
2020-05-12 16:33:20 +0530

    Added default ports and hosts

--- api/gateway/gateway/gateway/msgs.py
+++ api/gateway/gateway/gateway/msgs.py
@@ -6,13 +6,13 @@ TOKEN = "this_is_token"
 # logistic_service = "http://127.0.0.1:8002/"
 # order_service = "http://127.0.0.1:8003/"
 
-user_service_host = os.getenv("USER_SERVICE_SERVICE_HOST")
-logistic_service_host = os.getenv("LOGISTIC_SERVICE_SERVICE_HOST")
-order_service_host = os.getenv("ORDER_SERVICE_SERVICE_HOST")
+user_service_host = os.getenv("USER_SERVICE_SERVICE_HOST","127.0.0.1")
+logistic_service_host = os.getenv("LOGISTIC_SERVICE_SERVICE_HOST" , "127.0.0.1")
+order_service_host = os.getenv("ORDER_SERVICE_SERVICE_HOST" , "127.0.0.1")
 
-user_service_port = os.getenv("USER_SERVICE_SERVICE_PORT")
-logistic_service_port = os.getenv("LOGISTIC_SERVICE_SERVICE_PORT")
-order_service_port = os.getenv("ORDER_SERVICE_SERVICE_PORT")
+user_service_port = os.getenv("USER_SERVICE_SERVICE_PORT" , "9000")
+logistic_service_port = os.getenv("LOGISTIC_SERVICE_SERVICE_PORT" , "9000")
+order_service_port = os.getenv("ORDER_SERVICE_SERVICE_PORT" , "9000")
 
 user_service = user_service_host + ":" + user_service_port + "/"
 logistic_service = logistic_service_host + ":" + logistic_service_port + "/"
