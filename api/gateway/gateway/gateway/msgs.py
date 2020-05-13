22d8fb8f8b129eb6dbde96a91f5c22d9731326c5
Devansh Shah <43884681+dev1911@users.noreply.github.com>
2020-05-13 16:36:52 +0530

    Fixed host links

--- api/gateway/gateway/gateway/msgs.py
+++ api/gateway/gateway/gateway/msgs.py
@@ -14,8 +14,8 @@ user_service_port = os.getenv("USER_SERVICE_SERVICE_PORT" , "9000")
 logistic_service_port = os.getenv("LOGISTIC_SERVICE_SERVICE_PORT" , "9000")
 order_service_port = os.getenv("ORDER_SERVICE_SERVICE_PORT" , "9000")
 
-user_service = user_service_host + ":" + user_service_port + "/"
-logistic_service = logistic_service_host + ":" + logistic_service_port + "/"
-order_service = order_service_host + ":" + order_service_port + "/"
+user_service = "http://" + user_service_host + ":" + user_service_port + "/"
+logistic_service = "http://" + logistic_service_host + ":" + logistic_service_port + "/"
+order_service = "http://" + order_service_host + ":" + order_service_port + "/"
 
 token_error = "User is not authenticated"
